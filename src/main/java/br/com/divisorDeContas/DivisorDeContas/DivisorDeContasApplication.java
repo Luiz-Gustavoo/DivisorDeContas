e8acb365691775f65d67149f45e6975fa0cbd831
Luiz-Gustavoo <gustavo_luiz45@hotmail.com>
2025-09-18 20:50:30 -0300

    Commit inicial

---
+++ src/main/java/br/com/divisorDeContas/DivisorDeContas/DivisorDeContasApplication.java
@@ -0,0 +1,13 @@
+package br.com.divisorDeContas.DivisorDeContas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DivisorDeContasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DivisorDeContasApplication.class, args);
+	}
+
+}
