d1bc0c2245888d389e063fb945a973afd2de65b4
Luiz-Gustavoo <gustavo_luiz45@hotmail.com>
2025-09-18 20:49:57 -0300

    Commit inicial

---
+++ src/test/java/br/com/divisorDeContas/DivisorDeContas/DivisorDeContasApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.divisorDeContas.DivisorDeContas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DivisorDeContasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
