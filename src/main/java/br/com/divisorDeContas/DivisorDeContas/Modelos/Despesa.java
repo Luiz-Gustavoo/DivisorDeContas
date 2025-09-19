3c81f23e01ed6f0abf70094d970c9f3d5bb779c0
Luiz-Gustavoo <gustavo_luiz45@hotmail.com>
2025-09-18 21:29:26 -0300

    Adicionar classe Despesa

---
+++ src/main/java/br/com/divisorDeContas/DivisorDeContas/Modelos/Despesa.java
@@ -0,0 +1,45 @@
+package br.com.divisorDeContas.DivisorDeContas.Modelos;
+
+import java.util.Set;
+
+public class Despesa {
+    private String descricao;
+    private String categoria;
+    private double valor;
+
+    public void Despesa(String descricao, String categoria, double valor) {
+        this.descricao = descricao;
+        this.categoria = categoria;
+        this.valor = valor;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+    public String getCategoria() {
+        return categoria;
+    }
+
+    public void setCategoria(String categoria) {
+        this.categoria = categoria;
+    }
+
+    public double getValor() {
+        return valor;
+    }
+
+    public void setValor(double valor) {
+        this.valor = valor;
+    }
+
+    public String toString() {
+        return "Descrição: " + descricao + "\n" +
+                "Categoria: " + categoria + "\n" +
+                "Valor: " + valor;
+    }
+}
