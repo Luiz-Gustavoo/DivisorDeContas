4b45fcbd24017af68d76b8b52b38ab20e7628e6e
Luiz-Gustavoo <gustavo_luiz45@hotmail.com>
2025-09-18 22:13:21 -0300

    Adicionar validador de categoria

--- src/main/java/br/com/divisorDeContas/DivisorDeContas/Principal/Principal.java
+++ src/main/java/br/com/divisorDeContas/DivisorDeContas/Principal/Principal.java
@@ -1,9 +1,15 @@
 package br.com.divisorDeContas.DivisorDeContas.Principal;
 
+import br.com.divisorDeContas.DivisorDeContas.Modelos.Despesa;
+
+import java.util.Arrays;
+import java.util.HashSet;
 import java.util.Scanner;
+import java.util.Set;
 
 public class Principal {
-    Scanner leitor = new Scanner(System.in);
+    public Scanner leitor = new Scanner(System.in);
+    private Set<String> categoriasPermitidas = new HashSet<String>(Arrays.asList("necessidades", "desejos", "investimentos"));
 
     public void exibeMenu() {
         System.out.println("**********GERENCIADOR DE DESPESAS**********\n");
@@ -11,13 +17,20 @@ public class Principal {
         System.out.println("Informe a descrição: ");
         String descricaoDespesa = leitor.nextLine();
 
-        System.out.println("Informe a categoria dentre as seguintes opções: \n" +
-                "Necessidades \n" +
-                "Desejos \n" +
-                "Investimentos");
-        String categoriaDespesa = leitor.nextLine();
+        String categoriaDespesa = "";
+        while(!categoriasPermitidas.contains(categoriaDespesa)) {
+            System.out.println("Informe a categoria dentre as seguintes opções: \n" +
+                    "Necessidades \n" +
+                    "Desejos \n" +
+                    "Investimentos");
+
+            categoriaDespesa = leitor.nextLine();
+        }
 
         System.out.println("Informe o valor da despesa: ");
         double valorDespesa = leitor.nextDouble();
+
+        Despesa despesa = new Despesa(descricaoDespesa, categoriaDespesa, valorDespesa);
+        System.out.println(despesa);
     }
 }
