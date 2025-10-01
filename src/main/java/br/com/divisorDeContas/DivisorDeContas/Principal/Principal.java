02ec7ff6984e651546f80aef12ebff61369cb500
Luiz-Gustavoo <gustavo_luiz45@hotmail.com>
2025-09-30 22:06:36 -0300

    Adicionar menu de funcionalidades

--- src/main/java/br/com/divisorDeContas/DivisorDeContas/Principal/Principal.java
+++ src/main/java/br/com/divisorDeContas/DivisorDeContas/Principal/Principal.java
@@ -8,19 +8,42 @@ import br.com.divisorDeContas.DivisorDeContas.Modelos.Salario;
 import java.util.*;
 
 public class Principal {
+    Scanner leitor = new Scanner(System.in);
     public void exibeMenu() {
+        System.out.println("Escolha a funcionalidade desejada\n" +
+                "1 - Orçamento mensal\n" +
+                "2 - Gerenciador de despesas");
+
+        Integer funcionalidade = leitor.nextInt();
+
+        switch(funcionalidade){
+            case 1:
+                orcamentoMensal();
+                break;
+            case 2:
+                gerenciadorDeDespesa();
+                break;
+            default:
+                break;
+        }
+
+    }
+
+    private void orcamentoMensal(){
         System.out.println("**********ORÇAMENTO MENSAL**********\n");
 
         FormularioSalario formularioSalario = new FormularioSalario();
         Salario salario = formularioSalario.formularioSalario();
         salario.orcamento();
+    }
 
+    private void gerenciadorDeDespesa(){
         System.out.println("**********GERENCIADOR DE DESPESAS**********\n");
 
         FormularioDespesa formularioDespesa = new FormularioDespesa();
         List<Despesa> listaDeDespesas = formularioDespesa.formularioDespesa();
         System.out.println("Listagem de despesas: ");
         listaDeDespesas.forEach(System.out::println);
-
     }
+
 }
