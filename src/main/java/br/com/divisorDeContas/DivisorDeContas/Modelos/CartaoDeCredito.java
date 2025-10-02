8f654d5272aeeebdaa7c26473366278d6b89b6c0
Luiz-Gustavoo <gustavo_luiz45@hotmail.com>
2025-10-01 22:19:54 -0300

    Adicionar método getListaDespesas, sobrescrever o método formularioDespesa

--- src/main/java/br/com/divisorDeContas/DivisorDeContas/Modelos/CartaoDeCredito.java
+++ src/main/java/br/com/divisorDeContas/DivisorDeContas/Modelos/CartaoDeCredito.java
@@ -5,12 +5,15 @@ import java.util.List;
 import java.util.Scanner;
 
 public class CartaoDeCredito extends FormularioDespesa{
-    List<Despesa> listaDeDespesas = new ArrayList<>();
-    Scanner leitor = new Scanner(System.in);
     private double valorFatura;
 
     public CartaoDeCredito(double valorFatura){
         this.valorFatura = valorFatura;
+    }
+
+    public void getListaDeDespesas() {
+        System.out.println("\nDespesas da fatura: ");
+        listaDeDespesas.forEach(System.out::println);
     }
 
     public double getValorFatura(){
@@ -19,6 +22,7 @@ public class CartaoDeCredito extends FormularioDespesa{
 
     @Override
     public List<Despesa> formularioDespesa() {
+        System.out.println("Informe as despesas no formulário a seguir");
         return super.formularioDespesa();
     }
 }
